d3bcda6dd4e6c648bbae3de2c31139234146f337
bodhibrdr <julie.gumerman@gmail.com>
2019-09-06 09:39:16 -0600

    fetched the data and displayed it: MVP, part I, done

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(response => {
+        console.log(response);
+        this.setState({ data: response.data});
+        console.log(this.state, "data from state")
+
+      });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Go US Soccer!!!</h1>
+        <div>
+          {this.state.data.map(player => {
+            return (
+              <div key={player.id}>
+                <h3>{player.name}</h3>
+                <p>{player.country}</p>
+              </div>
+            );
+          
+          })}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
